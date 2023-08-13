0bbfb938dfda6f11710ecfb292529e5d653ceb79
Samuel Stark <samuel.winslow.stark@gmail.com>
2023-08-13 13:40:22 +0100

    Initial commit of VS CMake project

---
+++ realtime/DX12RealTimeRecolor/DX12RealTimeRecolor/DX12RealTimeRecolor.cpp
@@ -0,0 +1,12 @@
+﻿// DX12RealTimeRecolor.cpp : Defines the entry point for the application.
+//
+
+#include "DX12RealTimeRecolor.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
