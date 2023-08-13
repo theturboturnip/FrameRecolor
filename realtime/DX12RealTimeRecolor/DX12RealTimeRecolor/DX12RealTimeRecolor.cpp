b69149f53549c3b4f65a5cdbea06f2bf3633cded
Samuel Stark <samuel.winslow.stark@gmail.com>
2023-08-13 15:09:43 +0100

    Successfully create a window

--- realtime/DX12RealTimeRecolor/DX12RealTimeRecolor/DX12RealTimeRecolor.cpp
+++ realtime/DX12RealTimeRecolor/DX12RealTimeRecolor/DX12RealTimeRecolor.cpp
@@ -2,8 +2,162 @@
 //
 
 #include "DX12RealTimeRecolor.h"
+#include <algorithm>
+#include <memory>
 
-int main()
+using namespace RTR;
+
+struct Arguments {
+    LONG requestedWindowWidth;
+    LONG requestedWindowHeight;
+	bool enableWARP; // Windows Advanced Rasterization Platform - CPU supplementary rendering for older GPUs.
+};
+Arguments parse_command_line_args() {
+    auto args = Arguments{
+        .requestedWindowWidth = 1280,
+        .requestedWindowHeight = 720,
+        .enableWARP = false
+    };
+
+    // From https://www.3dgep.com/learning-directx-12-1
+    int argc;
+    wchar_t** argv = ::CommandLineToArgvW(::GetCommandLineW(), &argc);
+
+    for (size_t i = 0; i < argc; ++i)
+    {
+        if (::wcscmp(argv[i], L"-w") == 0 || ::wcscmp(argv[i], L"--width") == 0)
+        {
+            args.requestedWindowWidth = ::wcstol(argv[++i], nullptr, 10);
+        }
+        if (::wcscmp(argv[i], L"-h") == 0 || ::wcscmp(argv[i], L"--height") == 0)
+        {
+            args.requestedWindowHeight = ::wcstol(argv[++i], nullptr, 10);
+        }
+        if (::wcscmp(argv[i], L"-warp") == 0 || ::wcscmp(argv[i], L"--warp") == 0)
+        {
+            args.enableWARP = true;
+        }
+    }
+
+    // Free memory allocated by CommandLineToArgvW
+    ::LocalFree(argv);
+
+    return args;
+}
+
+// Global state
+std::unique_ptr<WindowState> g_windowState;
+std::unique_ptr<DX12State> g_dx12State;
+bool g_Initialized = false;
+
+// Window callback function.
+LRESULT CALLBACK window_message_callback(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    if (!g_Initialized) return ::DefWindowProc(hWnd, uMsg, wParam, lParam);
+    return ::DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+WindowState create_window(
+    const wchar_t* windowClassName,
+    HINSTANCE hInst,
+    const wchar_t* windowTitle,
+    LONG width,
+    LONG height
+) {
+    // Register a window class for creating our render window with.
+    WNDCLASSEXW windowClass = {
+        .cbSize = sizeof(WNDCLASSEX),
+        .style = CS_HREDRAW | CS_VREDRAW, // Always redraw the window if the width (HREDRAW) or height (VREDRAW) change
+        .lpfnWndProc = &window_message_callback,
+        .cbClsExtra = 0,
+        .cbWndExtra = 0,
+        .hInstance = hInst,
+        .hIcon = ::LoadIcon(hInst, NULL),
+        .hCursor = ::LoadCursor(NULL, IDC_ARROW),
+        .hbrBackground = (HBRUSH)(COLOR_WINDOW + 1),
+        .lpszMenuName = NULL,
+        .lpszClassName = windowClassName,
+        .hIconSm = ::LoadIcon(hInst, NULL)
+    };
+
+    static ATOM atom = ::RegisterClassExW(&windowClass);
+    assert(atom > 0);
+
+    // The class was successfully created, now create the window
+    int screenWidth = ::GetSystemMetrics(SM_CXSCREEN);
+    int screenHeight = ::GetSystemMetrics(SM_CYSCREEN);
+
+    RECT windowRect = {
+        .left=0,
+        .top=0,
+        .right=width,
+        .bottom=height
+    };
+    // Add padding to the window size for window frame and other stuff that goes outside the "client area".
+    ::AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+    int windowWidth = windowRect.right - windowRect.left;
+    int windowHeight = windowRect.bottom - windowRect.top;
+
+    // Center the window within the screen. Clamp to 0, 0 for the top-left corner.
+    int windowX = std::max<int>(0, (screenWidth - windowWidth) / 2);
+    int windowY = std::max<int>(0, (screenHeight - windowHeight) / 2);
+
+    HWND hWnd = ::CreateWindowExW(
+        NULL, // extended window style
+        windowClassName,
+        windowTitle,
+        WS_OVERLAPPEDWINDOW,
+        windowX,
+        windowY,
+        windowWidth,
+        windowHeight,
+        NULL, // parent window
+        NULL, // menu override
+        hInst,
+        nullptr
+    );
+
+    if (hWnd == NULL) {
+        ExitOnWin32Error(TEXT("CreateWindowExW"));
+    }
+
+    return WindowState{
+        .hWnd = hWnd,
+        .windowSizeWhenWindowed = {
+            .left = windowX,
+            .top = windowY,
+            .right = windowX + windowWidth,
+            .bottom = windowY + windowHeight
+        }
+    };
+}
+
+int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow)
 {
+    // Windows 10 Creators update adds Per Monitor V2 DPI awareness context.
+    // Using this awareness context allows the client area of the window 
+    // to achieve 100% scaling while still allowing non-client window content to 
+    // be rendered in a DPI sensitive fashion.
+    SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+
+    auto args = parse_command_line_args();
+
+#if defined(_DEBUG)
+    // Always enable the debug layer before doing anything DX12 related
+    // so all possible errors generated while creating DX12 objects
+    // are caught by the debug layer.
+    ComPtr<ID3D12Debug> debugInterface;
+    ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(&debugInterface)));
+    debugInterface->EnableDebugLayer();
+#endif
+
+    g_windowState = std::make_unique<WindowState>(create_window(
+        L"DX12WindowClass",
+        hInstance,
+        L"Real Time Recolor",
+        args.requestedWindowWidth,
+        args.requestedWindowHeight
+    ));
+
 	return 0;
 }
--- realtime/DX12RealTimeRecolor/DX12RealTimeRecolor/DX12RealTimeRecolor.h
+++ realtime/DX12RealTimeRecolor/DX12RealTimeRecolor/DX12RealTimeRecolor.h
@@ -3,6 +3,135 @@
 
 #pragma once
 
-#include <iostream>
+// Based on https://www.3dgep.com/learning-directx-12-1/
 
-// TODO: Reference additional headers your program requires here.
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <strsafe.h> // For StringCchPrintf
+#include <shellapi.h> // For CommandLineToArgvW
+
+// The min/max macros conflict with like-named member functions.
+// Only use std::min and std::max defined in <algorithm>.
+#if defined(min)
+#undef min
+#endif
+
+#if defined(max)
+#undef max
+#endif
+
+// In order to define a function called CreateWindow, the Windows macro needs to
+// be undefined.
+// SAMUEL NOTE - bad practice to make functions that overlap with windows macros anyway...
+#if defined(CreateWindow)
+#undef CreateWindow
+#endif
+
+// Windows Runtime Library. Needed for Microsoft::WRL::ComPtr<> template class.
+#include <wrl.h>
+// SAMUEL NOTE - The tutorial did `using namespace Microsoft::WRL;` but I prefer to only include the specific classes I need.
+template<typename T>
+using ComPtr = Microsoft::WRL::ComPtr<T>;
+
+// DirectX 12 specific headers.
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+// D3D12 extension library.
+// SAMUEL NOTE - Don't want to bother with installing this from https://github.com/microsoft/DirectX-Headers right now.
+//#include <d3dx12.h>
+
+#include <exception>
+// From DXSampleHelper.h 
+// Source: https://github.com/Microsoft/DirectX-Graphics-Samples
+inline void ThrowIfFailed(HRESULT hr)
+{
+    if (FAILED(hr))
+    {
+        throw std::exception();
+    }
+}
+// From https://learn.microsoft.com/en-us/windows/win32/debug/retrieving-the-last-error-code
+void ExitOnWin32Error(LPCTSTR lpszFunction)
+{
+    // Retrieve the system error message for the last-error code
+
+    LPVOID lpMsgBuf;
+    LPVOID lpDisplayBuf;
+    DWORD dw = GetLastError();
+
+    FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER |
+        FORMAT_MESSAGE_FROM_SYSTEM |
+        FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        dw,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPTSTR)&lpMsgBuf,
+        0, NULL);
+
+    // Display the error message and exit the process
+
+    lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
+        (lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR));
+    StringCchPrintf((LPTSTR)lpDisplayBuf,
+        LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+        TEXT("%s failed with error %d: %s"),
+        lpszFunction, dw, lpMsgBuf);
+    MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
+
+    LocalFree(lpMsgBuf);
+    LocalFree(lpDisplayBuf);
+    ExitProcess(dw);
+}
+
+namespace RTR {
+    using u32 = uint32_t;
+    using u64 = uint64_t;
+
+    constexpr u32 NUM_INFLIGHT_FRAMES = 3;
+
+    struct WindowState {
+        HWND hWnd;
+        // Used to remember what the width/height was before we went into fullscreen mode.
+        RECT windowSizeWhenWindowed;
+    };
+
+    struct DX12InflightFrameState {
+        ComPtr<ID3D12Resource> backBuffer;
+        ComPtr<ID3D12CommandAllocator> commandAllocator;
+        // Tracking for "is this frame in-flight?"
+        // If inflight is set, this frame has been passed to the GPU for rendering and we can't use the backbuffer or commandallocator on the CPU.
+        // valueForFrameFence is the value the inflightFrameFence will take when this frame is complete, setting inflight to false.
+        // valueForFrameFence only has meaning when inflight=true.
+        bool inflight;
+        u64 valueForFrameFence;
+    };
+
+    struct DX12State {
+        ComPtr<ID3D12Device2> device;
+        ComPtr<ID3D12CommandQueue> commandQueue;
+
+        // Swapchain and associated configuration
+        ComPtr<IDXGISwapChain4> swapchain;
+        bool swapchainVsync;
+        bool swapchainTearingSupported; // ?
+        bool swapchainFullscreen;
+
+        // The heap for storing render target descriptors.
+        // The size of a render target descriptor is vendor-dependent.
+        ComPtr<ID3D12DescriptorHeap> renderTargetDescriptorHeap;
+        UINT renderTargetDescriptorSize;
+
+        ComPtr<ID3D12CommandList> commandList; // Used for recording commands, we only ever need one at a time
+
+        ComPtr<ID3D12Fence> inflightFrameFence;
+        u64 inflightFrameFenceValue;
+        HANDLE inflightFrameFenceEvent;
+
+        DX12InflightFrameState perFrameState[NUM_INFLIGHT_FRAMES];
+        UINT currentInflightFrame;
+    };
+}
